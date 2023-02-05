c011d1b1cdeb6d603d0899d3c73a925f2c3a7c77
Zaid <iamzaidsheikh@gmail.com>
2023-02-05 18:26:08 +0530

    refactor(runner): move the runner logic to lib

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
+
+use poorly_written_cli_app::Config;
 
 fn main() {
 
@@ -17,54 +17,8 @@ fn main() {
     println!("In file: {}", config.file_path);
 
     // not unwrapping because return type is unit
-    if let Err(e) = run(config) {
+    if let Err(e) = poorly_written_cli_app::run(config) {
         println!("Application error: {e}");
         process::exit(1);
-    }
-}
-
-/// Prints the contents of file in the `config` if the result is success
-/// else returns an `Error` if unable to parse the file to string.
-///
-/// # Arguments
-///
-/// * `config` - A config struct that holds the parsed command line arguments
-///
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    // ? will throw an error instead of panicking
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("With text:\n{contents}");
-
-    Ok(())
-}
-
-/// Config passed in as command line arguments when running the program
-///
-/// * `query` - search query
-/// * `file_path` - path of the file to search in
-///
-struct Config {
-    query: String,
-    file_path: String
-}
-
-impl Config {
-    /// Returns a `Result` containing the `config` if `args` are parsed successfully
-    /// else returns an `Err` if length of `args` is less than three.
-    ///
-    /// # Arguments
-    ///
-    /// * `args` - A string slice that holds the command line arguments passed
-    ///
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments")
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
     }
 }
